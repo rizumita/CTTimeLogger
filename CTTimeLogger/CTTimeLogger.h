8a3d3016944669b5d45e87773fca0c886015b395
Ryoichi Izumita <r.izumita@caph.jp>
2013-04-01 10:06:43 +0900

    Initial Commit

---
+++ CTTimeLogger/CTTimeLogger.h
@@ -0,0 +1,13 @@
+//
+//  CTTimeLogger.h
+//  CTTimeLogger
+//
+//  Created by 和泉田 領一 on 2013/04/01.
+//  Copyright (c) 2013年 CAPH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTTimeLogger : NSObject
+
+@end
---
+++ CTTimeLogger/CTTimeLogger.m
@@ -0,0 +1,13 @@
+//
+//  CTTimeLogger.m
+//  CTTimeLogger
+//
+//  Created by 和泉田 領一 on 2013/04/01.
+//  Copyright (c) 2013年 CAPH. All rights reserved.
+//
+
+#import "CTTimeLogger.h"
+
+@implementation CTTimeLogger
+
+@end
